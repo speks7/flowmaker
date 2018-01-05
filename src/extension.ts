47b07e1957b7823e802ffcd783df4af7faa447ae
Sunim Acharya <sunim.54@gmail.com>
2018-01-05 10:56:30 +0545

    Add js2flowchart

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as js2flowchart from 'js2flowchart';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
