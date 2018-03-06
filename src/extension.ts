0d2cf32bd88dca6ab86f35dc07b3f2aa4aba5d36
Sunim Acharya <sunim.54@gmail.com>
2018-03-07 00:25:38 +0545

    Import files

--- src/extension.ts
+++ src/extension.ts
@@ -4,11 +4,15 @@ import * as HTTP from "http";
 import * as express from "express";
 import * as socket from "socket.io";
 import * as js2flowchart from "js2flowchart";
+import * as path from "path";
 import { window, commands, Uri } from "vscode";
 import { dirname } from "path";
+import { writeFileSync } from "fs";
+import { homedir } from "os";
 import { ExtensionConstants } from "./constants";
 import { FlowOnBrowser } from "./onBrowserManager";
 import { TextDocumentContentProvider } from "./onEditorManager";
+
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 
@@ -17,14 +21,41 @@ import { TextDocumentContentProvider } from "./onEditorManager";
 let flowBrowser: FlowOnBrowser;
 let flowVS: TextDocumentContentProvider;
 //let flowVSCpde:
+
+const writeSerializedBlobToFile = (serializeBlob, fileName) => {
+  const bytes = new Uint8Array(serializeBlob.split(","));
+  writeFileSync(fileName, new Buffer(bytes));
+};
+
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log("active!");
 
+  let lastUsedImageUri = vscode.Uri.file(
+    path.resolve(homedir(), "Desktop/code.png")
+  );
+
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
+
+  vscode.commands.registerCommand("extension.Saver", serializedBlob => {
+    vscode.window
+      .showSaveDialog({
+        defaultUri: lastUsedImageUri,
+        filters: {
+          Images: ["png"]
+        }
+      })
+      .then(uri => {
+        if (uri) {
+          writeSerializedBlobToFile(serializedBlob, uri.fsPath);
+          lastUsedImageUri = uri;
+        }
+      });
+  });
+
   let disposableBrowser = vscode.commands.registerCommand(
     "extension.onBrowser",
     () => {
