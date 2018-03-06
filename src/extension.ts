751e76e45563abcaba01dc196264739356493987
Sunim Acharya <sunim.54@gmail.com>
2018-03-07 00:18:49 +0545

    Format typescripts

--- src/extension.ts
+++ src/extension.ts
@@ -1,67 +1,77 @@
-'use strict';
-import * as vscode from 'vscode';
-import * as HTTP from 'http';
-import * as express from 'express' ;
-import * as socket from 'socket.io';
-import * as js2flowchart from 'js2flowchart';
-import {window,commands, Uri} from 'vscode';
-import { dirname } from 'path';
-import {ExtensionConstants} from './constants';
-import {FlowOnBrowser} from './onBrowserManager'
-import {TextDocumentContentProvider} from './onEditorManager';
+"use strict";
+import * as vscode from "vscode";
+import * as HTTP from "http";
+import * as express from "express";
+import * as socket from "socket.io";
+import * as js2flowchart from "js2flowchart";
+import { window, commands, Uri } from "vscode";
+import { dirname } from "path";
+import { ExtensionConstants } from "./constants";
+import { FlowOnBrowser } from "./onBrowserManager";
+import { TextDocumentContentProvider } from "./onEditorManager";
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-let flowBrowser : FlowOnBrowser;
-let flowVS : TextDocumentContentProvider;
+let flowBrowser: FlowOnBrowser;
+let flowVS: TextDocumentContentProvider;
 //let flowVSCpde:
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log("active!");
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('active!');
-    
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposableBrowser = vscode.commands.registerCommand('extension.onBrowser', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        flowBrowser=new FlowOnBrowser();
-        vscode.window.showInformationMessage('Server started successfully on http:localhost:'+ExtensionConstants.PORT);
-    });
-    let disposableOnVSCode = vscode.commands.registerCommand("extension.onEditor",() => {
-        flowVS = new TextDocumentContentProvider();
-        let registration = vscode.workspace.registerTextDocumentContentProvider("flowmaker", flowVS);
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposableBrowser = vscode.commands.registerCommand(
+    "extension.onBrowser",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      flowBrowser = new FlowOnBrowser();
+      vscode.window.showInformationMessage(
+        "Server started successfully on http:localhost:" +
+          ExtensionConstants.PORT
+      );
+    }
+  );
+  let disposableOnVSCode = vscode.commands.registerCommand(
+    "extension.onEditor",
+    () => {
+      flowVS = new TextDocumentContentProvider();
+      let registration = vscode.workspace.registerTextDocumentContentProvider(
+        "flowmaker",
+        flowVS
+      );
 
-        vscode.workspace.onDidChangeTextDocument(
-            (caught: vscode.TextDocumentChangeEvent) => {
-              if (caught.document === vscode.window.activeTextEditor.document) {
-                flowVS.update(ExtensionConstants.liveURI);
-              }
-            }
-          );
-          vscode.window.onDidChangeTextEditorSelection(
-            (caught: vscode.TextEditorSelectionChangeEvent) => {
-              if (caught.textEditor === vscode.window.activeTextEditor) {
-                flowVS.update(ExtensionConstants.liveURI);
-              }
-            }
-          );
-          return vscode.commands.executeCommand("vscode.previewHtml",
-                            ExtensionConstants.liveURI,
-                            vscode.ViewColumn.Two,
-                            "flowmaker"
-                             );
-        
-        });
-    context.subscriptions.push(disposableBrowser);
-    context.subscriptions.push(disposableOnVSCode);
+      vscode.workspace.onDidChangeTextDocument(
+        (caught: vscode.TextDocumentChangeEvent) => {
+          if (caught.document === vscode.window.activeTextEditor.document) {
+            flowVS.update(ExtensionConstants.liveURI);
+          }
+        }
+      );
+      vscode.window.onDidChangeTextEditorSelection(
+        (caught: vscode.TextEditorSelectionChangeEvent) => {
+          if (caught.textEditor === vscode.window.activeTextEditor) {
+            flowVS.update(ExtensionConstants.liveURI);
+          }
+        }
+      );
+      return vscode.commands.executeCommand(
+        "vscode.previewHtml",
+        ExtensionConstants.liveURI,
+        vscode.ViewColumn.Two,
+        "flowmaker"
+      );
+    }
+  );
+  context.subscriptions.push(disposableBrowser);
+  context.subscriptions.push(disposableOnVSCode);
 }
 // this method is called when your extension is deactivated
 export function deactivate() {
-    if(flowBrowser)
-        flowBrowser.stopServer();
+  if (flowBrowser) flowBrowser.stopServer();
 }
--- src/onEditorManager.ts
+++ src/onEditorManager.ts
@@ -3,33 +3,33 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import * as js2flowchart from "js2flowchart";
-import {ExtensionConstants} from './constants';
+import { ExtensionConstants } from "./constants";
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
 export class TextDocumentContentProvider
-    implements vscode.TextDocumentContentProvider {
-    private _onDidChange = new vscode.EventEmitter<vscode.Uri>();
+  implements vscode.TextDocumentContentProvider {
+  private _onDidChange = new vscode.EventEmitter<vscode.Uri>();
 
-    public provideTextDocumentContent(uri: vscode.Uri): string {
-      return this.JsRenderView();
-    }
-
-    get onDidChange(): vscode.Event<vscode.Uri> {
-      return this._onDidChange.event;
-    }
-
-    public update(uri: vscode.Uri) {
-      this._onDidChange.fire(uri);
-    }
-
-    private JsRenderView() {
-      let WindowP = vscode.window.activeTextEditor;
-      if (!(WindowP.document.languageId === "javascript")) {
-        return "Only .js file is supported";
-      }
-      let text = WindowP.document.getText();
-      const svg = js2flowchart.convertCodeToSvg(text);
-      return `<body style="background-color:white;">${svg}</body>`;
-    }
+  public provideTextDocumentContent(uri: vscode.Uri): string {
+    return this.JsRenderView();
   }
+
+  get onDidChange(): vscode.Event<vscode.Uri> {
+    return this._onDidChange.event;
+  }
+
+  public update(uri: vscode.Uri) {
+    this._onDidChange.fire(uri);
+  }
+
+  private JsRenderView() {
+    let WindowP = vscode.window.activeTextEditor;
+    if (!(WindowP.document.languageId === "javascript")) {
+      return "Only .js file is supported";
+    }
+    let text = WindowP.document.getText();
+    const svg = js2flowchart.convertCodeToSvg(text);
+    return `<body style="background-color:white;">${svg}</body>`;
+  }
+}
